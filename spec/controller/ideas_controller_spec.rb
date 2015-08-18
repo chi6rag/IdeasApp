fb5f327cc91265d91b5b27622e73f8bdc767a80b
Chirag Aggarwal <chi6rag@gmail.com>
2015-08-18 14:48:42 +0530

    - Add FactoryGirl code for generating invalid idea - Add ideas_controller specs for index, show, edit and new controller actions

--- spec/controller/ideas_controller_spec.rb
+++ spec/controller/ideas_controller_spec.rb
@@ -1,30 +1,76 @@
 require 'spec_helper'
 
-describe IdeasController do 
-	describe "GET #index" do 
-		it "renders the index view"
-		it "shows a list if ideas"
+describe IdeasController, type: :controller do 
+	describe "GET #index" do
+		
+		it "renders the index view" do
+			get :index
+			expect(response).to render_template :index
+		end
+		
+		it "shows a list if ideas" do 
+			idea =  FactoryGirl.create(:idea)
+			get :index
+			expect(assigns(:ideas)).to eq([idea])
+		end
+
 	end
 
 	describe "GET #show" do 
-		it "renders the show template"
-		it "assigns the idea as @idea"
+		
+		before :each do 
+			@idea = FactoryGirl.create(:idea)
+			get :show, id: @idea.id
+		end
+		
+		it "renders the show template" do 
+			expect(response).to render_template(:show)
+		end
+
+		it "assigns the idea as @idea" do
+			expect(assigns(:idea)).to eq(@idea)
+		end
+
 	end
 
 	describe "GET #new" do 
-		it "renders the new idea page"
-		it "assigns a new idea as @idea"
+		
+		it "renders the new idea page" do 
+			get :new
+			expect(response).to render_template(:new)
+		end
+
+		it "assigns a new idea as @idea" do 
+			get :new
+			expect(assigns(:idea)).to be_a_new(Idea)
+		end
+
 	end
 
 	describe "GET #edit" do 
-		it "renders the edit idea page"
-		it "assigns a idea as @idea"
+		
+		before :each do 
+			@idea = FactoryGirl.create(:idea)
+			get :edit, id: @idea.id
+		end
+
+		it "renders the edit idea page" do 
+			expect(response).to render_template(:edit)
+		end
+
+		it "assigns a idea as @idea" do
+			expect(assigns(:idea)).to eq(@idea)
+		end
+
 	end
 
 	describe "POST #create" do 
 		context "with valid attributes" do 
+			
 			it "saves the new idea into the database"
+
 			it "redirects to the idea"
+
 		end
 		context "with invalid attributes" do 
 			it "does not save the new idea into the database"
--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -4,4 +4,8 @@ FactoryGirl.define do
 	factory :idea do |f|
 		f.name { Faker::Company.name }
 	end
+
+	factory :invalid_idea, parent: :idea do |f|
+		f.name nil
+	end
 end
